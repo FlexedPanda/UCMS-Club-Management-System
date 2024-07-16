37f8884dedf1642d95298bae498ccbf7fd2b3d2a
FlexedPanda <77376571+FlexedPanda@users.noreply.github.com>
2024-07-16 13:58:01 +0600

    Update club.sql

--- misc/club.sql
+++ misc/club.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2024 at 03:23 PM
+-- Generation Time: Apr 17, 2024 at 03:20 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
